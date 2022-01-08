18a21e97eb9040cb75934891e50a35c7ec80ca48
maulik8789 <maulik.vagh@gmail.com>
2022-01-08 23:08:47 +0530

    added Session

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "m"
-
-responses = []
+debug = DebugToolbarExtension(app)
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 @app.route('/')
+def start():
+    
+    return redirect ('/begin_survey')
+
+@app.route('/begin_survey')
 def home_page():
-    responses = []
+    session["responses"] = []
     title = satisfaction_survey.title
     instruction = satisfaction_survey.instructions
     return render_template('home.html', 
@@ -19,19 +24,22 @@ def home_page():
 @app.route('/answer', methods=["POST"])
 def add_ans():
     add_res = request.form['choice']
-    responses.append(add_res)
+    resp = session["responses"]
+    resp.append(add_res)
+    session["responses"] = resp
 
-    if len(responses) >= len(satisfaction_survey.questions):
-        reponses = []  
+
+    if len(resp) >= len(satisfaction_survey.questions):
+        #session[“responses”] = []  
         return render_template('/thanks.html')
     else:
-        return redirect(f"/questions/{len(responses)}")
+        return redirect(f"/questions/{len(resp)}")
 
 @app.route('/questions/<int:q_num>')
 def que(q_num):
-
-    if len(responses) != q_num:
-        reponses = []
+    resp = session["responses"]
+    if len(resp) != q_num:
+        session["responses"] = []
         flash(f"invalid question number")
         return render_template("/thanks.html")
     
